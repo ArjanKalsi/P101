64785382aad64132fdcaaef7876934151e29182b
ArjanKalsi <109098789+ArjanKalsi@users.noreply.github.com>
2023-06-30 18:48:19 +0100

    Add files via upload

---
+++ roll-a-dice.py
@@ -0,0 +1,50 @@
+import random
+
+response = "y"
+
+while response == "y":
+    no = random.randint(1,6)
+
+    if no == 1:
+        print("---------")
+        print("|       |")
+        print("|   •   |")
+        print("|       |")
+        print("---------")
+
+    elif no == 2:
+        print("---------")
+        print("| •     |")
+        print("|       |")
+        print("|     • |")
+        print("---------")
+
+    elif no == 3:
+        print("---------")
+        print("| •     |")
+        print("|   •   |")
+        print("|     • |")
+        print("---------")
+
+    elif no == 4:
+        print("---------")
+        print("| •   • |")
+        print("|       |")
+        print("| •   • |")
+        print("---------")
+
+    elif no == 5:
+        print("---------")
+        print("| •   • |")
+        print("|   •   |")
+        print("| •   • |")
+        print("---------")
+
+    else:
+        print("---------")
+        print("| •   • |")
+        print("| •   • |")
+        print("| •   • |")
+        print("---------")
+
+    response = input("Do you want to roll again? (y/n)")
